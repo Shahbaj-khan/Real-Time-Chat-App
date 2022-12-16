390f4e9a1b14e48b9dc8c766f2ed76bc5802a326
Shahbaj Khan <shahbajkhan8293@gmail.com>
2022-12-17 04:58:02 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jr9hn2h20ie4=r0_jpb11dwgf0h)v-^wk!!al*%l*tz-odv3$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,6 +125,3 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
